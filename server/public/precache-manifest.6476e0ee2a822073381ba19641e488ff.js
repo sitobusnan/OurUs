b834fa73394c653430fa91a54db14517c80935df
sitobusnan <sito@citytonight.es>
2018-12-17 14:06:32 +0100

    2nd heroku

---
+++ server/public/precache-manifest.6476e0ee2a822073381ba19641e488ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cd0484b3006185a5dc2",
+    "url": "/static/js/main.4cd0484b.chunk.js"
+  },
+  {
+    "revision": "eeb299fac1ee2fdbc303",
+    "url": "/static/js/1.eeb299fa.chunk.js"
+  },
+  {
+    "revision": "4cd0484b3006185a5dc2",
+    "url": "/static/css/main.5489901d.chunk.css"
+  },
+  {
+    "revision": "eeb299fac1ee2fdbc303",
+    "url": "/static/css/1.0a7e1c19.chunk.css"
+  },
+  {
+    "revision": "c60951ee089cb3c43d021642d3433422",
+    "url": "/index.html"
+  }
+];
